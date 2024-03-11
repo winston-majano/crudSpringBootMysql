cc3c1d7f4d8f7ed361a0b8100c25d93700f1c119
winston-majano <winstonmajano@gmail.com>
2024-03-11 18:35:45 +0100

    subiendo estructura de proyecto crud con mysql, jpa y thymelaft

---
+++ src/main/java/com/wm/crudSpringBootMysql/CrudSpringBootMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.wm.crudSpringBootMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wm/crudSpringBootMysql/CrudSpringBootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wm.crudSpringBootMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
